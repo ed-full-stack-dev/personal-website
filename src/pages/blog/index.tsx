349939f16ed0942063dfa64fb0cadf35ee670905
ed-full-stack-dev <ed.fullstack.dev@gmail.com>
2024-05-13 17:07:25 -0300

    basic pages and navigation added

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from '../../components/navigation'
+
+function Blog() {
+  return (
+    <div>
+        <Navigation />
+      <h1>Blogs List</h1>
+    </div>
+  ) 
+}
+
+export default Blog
